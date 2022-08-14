fb37417457ca3e519e5e6eda5f3d6fa8136d48cd
fabs1902 <fabiolamartinez190201@gmail.com>
2022-08-13 20:28:44 -0600

    primer version

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import './App.css';
+import Form from "./components/Form";
+
+const App = () => {
+  return (
+    <div className="App">
+      <div className="App-content">
+        <p>
+          Aquí esta el TO-DO list
+        </p>
+        <Form/>
+      </div>
+    </div>
+  )
+  
+}
+export default App;
